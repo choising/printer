d57615bcc5c85bb12ea0a445e07a0dcc057b9a9d
choising <seungmin.choi@woowahan.com>
2019-11-21 14:52:03 +0900

    first commit

---
+++ src/test/java/com/baemin/touchorder/printer/PrinterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baemin.touchorder.printer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
